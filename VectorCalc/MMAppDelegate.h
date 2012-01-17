f86f399d297533fa1aeed31692179c6e13915b9c
Matthew Murdock <mtmurdock@155-99-212-166.uconnect.utah.edu>
2012-01-17 14:19:01 -0700

    Initial Commit

---
+++ VectorCalc/MMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MMAppDelegate.h
+//  VectorCalc
+//
+//  Created by Matthew Murdock on 1/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VectorCalc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VectorCalc
+//
+//  Created by Matthew Murdock on 1/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MMAppDelegate class]));
+    }
+}
